9ef511aee11f04e984c49ab00ba622889ec7c0a1
mao iwasaki <103989956+monchy1017@users.noreply.github.com>
2023-02-19 16:54:24 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+import math
+from helpers import apology, login_required, lookup, usd
+import sqlite3
+
+app = Flask(__name__)
+
+# データベースに接続
+conn = sqlite3.connect('board.db')
+c = conn.cursor()
+
