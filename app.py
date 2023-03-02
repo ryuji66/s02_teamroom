a51ce77e622adeb3d79f1e27b74d31fd3e1b8420
manesna361 <91590938+manesna361@users.noreply.github.com>
2023-03-02 18:49:19 +0900

    flash文、registerの実装を行いました。dbには型のエラーが出ています（さかな）

--- app.py
+++ app.py
@@ -7,11 +7,16 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
 import math
-#from helpers import apology, login_required, lookup, usd
+#from helpers import apology, login_required
 import sqlite3
 from flask import current_app, g
 
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)フラッシュ表示を有効にするために必要だった
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # データベース(sqlite3)に接続
 conn = sqlite3.connect('board.db')
@@ -77,32 +82,42 @@ def input():
         # projectが入力されてるか確認
         if not get_project:
             flash("プロジェクト名を入力してください")
+            return redirect('input')
 
         # mailが入力されてるか確認
         elif not get_mail:
             flash("メールアドレスを入力してください")
+            return redirect('input')
 
         # languageが入力されてるか確認
         elif not get_language:
             flash("プログラミング言語を入力してください")
+            return redirect('input')
 
         # genreが入力されてるか確認
         elif not get_genre:
             flash("ジャンルを入力してください")
+            return redirect('input')
 
         # periodが入力されてるか確認
         elif not get_period:
             flash("募集期間を入力してください")
+            return redirect('input')
 
         # completeが入力されてるか確認
         elif not get_complete:
             flash("制作期間を入力してください")
+            return redirect('input')
 
         # personが入力されてるか確認
         elif not get_person:
             flash("募集する人のレベルを入力してください")
+            return redirect('input')
 
-        # db.execute("INSERT INTO entry (title, mail_address, time, level, day_posted, day_end, body) values(?, ?, ?, ?, ?, ?, ?)", get_project, get_mail, get_complete, get_person, get_posted, get_period, get_text)
+        g.db.execute("INSERT INTO entries (title, mail_address, time, level, genre, day_posted, day_end, body) values (?, ?, ?, ?, ?, ?, ?, ?)", (get_project, get_mail, get_complete, get_person, get_genre, get_posted, get_period, get_text))
+        # 工夫しがいがありそう
+        get_entryid = g.db.execute("SELECT entry_id FROM entries WHERE title = ? AND mail_address = ? AND time = ? AND level = ? AND genre = ? AND day_posted = ? AND day_end = ? AND body = ?", (get_project, get_mail, get_complete, get_person, get_genre, get_posted, get_period, get_text))
+        g.db.execute("INSERT INTO language_to_entry (language_id, entry_id) values(?, ?)", (get_language, get_entryid))
 
         return render_template("index.html")
 
@@ -113,7 +128,51 @@ def input():
 def register():
     if request.method == "POST":
 
-        return redirect("/")
+        get_username = request.form.get("username")
+        get_password = request.form.get("password")
+        get_again = request.form.get("confirmation")
+
+        # usernameが入力されているか確認
+        if not get_username:
+            flash("ユーザーネームを入力してください")
+            return redirect("register")
+
+        # passwordが入力されているか確認
+        elif not get_password:
+            flash("パスワードを入力してください")
+            return redirect("register")
+
+        # confirmationが入力されているか確認
+        elif not get_again:
+            flash("パスワードを再入力してください")
+            return redirect("register")
+
+        # パスワードと再入力の一致を確認
+        elif get_password != get_again:
+            flash("パスワードと再入力は一致させてください")
+            return redirect("register")
+
+        # Query database for username
+        rows = g.db.execute("SELECT * FROM users WHERE username = ?", get_username)
+
+        # ユーザーネームが一つしかないか確認する
+        if len(rows) == 1:
+            flash("そのユーザーネームは既に使用されています")
+            return redirect("register")
+
+        # そのユーザーネームが登録されていなければ
+        if len(rows) != 1:
+
+            # パスワードをハッシュ化する
+            hash_password = generate_password_hash(get_password)
+
+        # 新規登録処理
+        user_id = g.db.execute("INSERT INTO users(username, hash) values(?, ?)", get_username, hash_password)
+
+        # ログイン状態にする
+        session["user_id"] = user_id
+
+        return redirect("register")
 
     else:
         return render_template("register.html")
