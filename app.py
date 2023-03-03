cd457745e011a29e5c59539eb2f6d0eb478776da
mao iwasaki <103989956+monchy1017@users.noreply.github.com>
2023-03-03 11:04:20 +0000

    Fri, Mar 3, 2023, 8:04 PM +09:00

--- app.py
+++ app.py
@@ -186,13 +186,6 @@ def login():
     session.clear()
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-        # Query database for username
         #*Loginを実現している
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
         # Ensure username exists and password is correct
