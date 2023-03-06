d42883aa799f788ba79642ba62e3505812fe3b52
mao iwasaki <103989956+monchy1017@users.noreply.github.com>
2023-03-06 08:43:47 +0000

    Mon, Mar 6, 2023, 5:43 PM +09:00

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ db = SQL("sqlite:///board.db")
 @app.route("/")
 def index():
     # entriesテーブルと中間テーブルを結合して必要な情報を取得
-    rows = db.execute("SELECT * FROM entries")
+    rows = db.execute("SELECT entries.*, languages.name FROM entries JOIN language_to_entry ON entries.entry_id = language_to_entry.entry_id JOIN languages ON language_to_entry.language_id = languages.language_id")
 
     return render_template('index.html', entries=rows)
 
