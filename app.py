79163a92b52d0ce9d0dde4112130e94c9514905c
manesna361 <91590938+manesna361@users.noreply.github.com>
2023-03-06 12:53:00 +0000

    Mon, Mar 6, 2023, 9:53 PM +09:00

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import os
+# import os
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
+# from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
 # import math
