9fe15be7726da5126fd24419181858566c0815be
manesna361 <91590938+manesna361@users.noreply.github.com>
2023-03-11 19:21:17 +0900

    マイページの実装、output,watchなどの実装も行った

--- app.py
+++ app.py
@@ -93,7 +93,6 @@ def input():
         get_complete = request.form.get("complete")
         get_person = request.form.get("person")
         get_text = request.form.get("text")
-        print(session["user_id"])
 
         # projectが入力されてるか確認
         if not get_project:
@@ -249,21 +248,160 @@ def logout():
 @app.route("/mypage", methods=["GET", "POST"])
 @login_required
 def mypage():
-    user_id = session.get('user_id', None)
+    # entriesテーブルと中間テーブルを結合して必要な情報を取得
+    entries = db.execute("""
+        SELECT entries.*, GROUP_CONCAT(languages.name) as language_name, users.username
+        FROM entries
+        LEFT JOIN language_to_entry
+        ON entries.entry_id = language_to_entry.entry_id
+        LEFT JOIN languages
+        ON languages.language_id = language_to_entry.language_id
+        LEFT JOIN users
+        ON users.user_id = entries.user_id
+        WHERE is_active = 1
+        AND entries.user_id = ?
+        GROUP BY entries.entry_id;
+    """, session["user_id"])
 
 
     if request.method == "POST":
+        db.execute("DELETE FROM language_to_entry WHERE entry_id = ?", request.form.get("deleteid"))
+        db.execute("DELETE FROM entries WHERE entry_id = ?", request.form.get("deleteid"))
 
-        return redirect("/")
+        entries = db.execute("""
+            SELECT entries.*, GROUP_CONCAT(languages.name) as language_name, users.username
+            FROM entries
+            LEFT JOIN language_to_entry
+            ON entries.entry_id = language_to_entry.entry_id
+            LEFT JOIN languages
+            ON languages.language_id = language_to_entry.language_id
+            LEFT JOIN users
+            ON users.user_id = entries.user_id
+            WHERE is_active = 1
+            AND entries.user_id = ?
+            GROUP BY entries.entry_id;
+        """, session["user_id"])
+
+        return render_template("mypage.html", entries = entries)
 
     else:
-        return render_template("mypage.html")
+        return render_template("mypage.html", entries = entries)
+
+@app.route("/output", methods=["GET", "POST"])
+@login_required
+def output():
+    if request.method == "POST":
+
+        get_project = request.form.get("project")
+        get_mail = request.form.get("mail")
+        get_languagelist = request.form.getlist("language")
+        get_genre = request.form.get("genre")
+        get_posted = datetime.datetime.now().date()
+        get_period = str(request.form.get("period"))
+        get_complete = request.form.get("complete")
+        get_person = request.form.get("person")
+        get_text = request.form.get("text")
+
+        # projectが入力されてるか確認
+        if not get_project:
+            flash("プロジェクト名を入力してください")
+            return redirect('output')
+
+        # mailが入力されてるか確認
+        elif not get_mail:
+            flash("メールアドレスを入力してください")
+            return redirect('output')
+
+        # languageが入力されてるか確認
+        elif not get_languagelist:
+            flash("プログラミング言語を入力してください")
+            return redirect('output')
+
+        # genreが入力されてるか確認
+        elif not get_genre:
+            flash("ジャンルを入力してください")
+            return redirect('output')
+
+        # periodが入力されてるか確認
+        elif not get_period:
+            flash("募集期間を入力してください")
+            return redirect('output')
+
+        # completeが入力されてるか確認
+        elif not get_complete:
+            flash("制作期間を入力してください")
+            return redirect('output')
+
+        # personが入力されてるか確認
+        elif not get_person:
+            flash("募集する人のレベルを入力してください")
+            return redirect('output')
+
+        # textが入力されてるか確認
+        elif not get_text:
+            flash("テキストを何か入力してください")
+            return redirect('output')
+
+        db.execute("INSERT INTO entries (title, mail_address, time, level, genre, day_posted, day_end, body, user_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?)", get_project, get_mail, get_complete, get_person, get_genre, get_posted, get_period, get_text, session["user_id"])
+        # 工夫しがいがありそう
+        get_entryid = db.execute("SELECT entry_id FROM entries WHERE title = ? AND mail_address = ? AND time = ? AND level = ? AND genre = ? AND day_posted = ? AND day_end = ? AND body = ?", get_project, get_mail, get_complete, get_person, get_genre, get_posted, get_period, get_text)
+
+        for i in get_languagelist:
+            get_language = int(i)
+            db.execute("INSERT INTO language_to_entry (language_id, entry_id) values(?, ?)", get_language, get_entryid[0]["entry_id"])
+
+        return render_template('watch.html')
+
+    else:
+        return render_template("output.html")
+
+@app.route("/watch")
+def watch():
+    # タグボタンの表示名を表示させるためにデータを取ってきてる
+    genres = db.execute("SELECT * FROM genres")
+    # その他を削除する
+    genres.pop()
+
+    # 上と同じ
+    languages = db.execute("SELECT * FROM languages")
+    # その他を削除する
+    languages.pop()
+
+    checking = db.execute("SELECT entry_id, day_end FROM entries")
+    get_today = str(datetime.datetime.now().date())
+
+    for i,j in enumerate(checking):
+        # noneの場合は本来ないがテストデータがnoneになってしまっているため導入
+        if checking[i]["day_end"] != None:
+            # 今日と締切日を比較し締切日を過ぎていればis_activeを0にする
+            formatted_date1 = time.strptime(str(checking[i]["day_end"]), "%Y-%m-%d")
+            formatted_date2 = time.strptime(get_today, "%Y-%m-%d")
+            if formatted_date1 < formatted_date2:
+                db.execute("UPDATE entries SET is_active = 0 WHERE entry_id = ?", checking[i]["entry_id"])
+
+    # entriesテーブルと中間テーブルを結合して必要な情報を取得
+    entries = db.execute("""
+        SELECT entries.*, GROUP_CONCAT(languages.name) as language_name, users.username
+        FROM entries
+        LEFT JOIN language_to_entry
+        ON entries.entry_id = language_to_entry.entry_id
+        LEFT JOIN languages
+        ON languages.language_id = language_to_entry.language_id
+        LEFT JOIN users
+        ON users.user_id = entries.user_id
+        WHERE is_active = 0
+        GROUP BY entries.entry_id;
+    """)
+
+    #print(entry_languages)
+
+    return render_template('watch.html', genres=genres, languages=languages, entries=entries)
 
 
 
 @app.route("/index_tag/<string:tag>")
 def index_tag(tag):
-    print(tag)
+
     # タグボタンの表示名を表示させるためにデータを取ってきてる
     genres = db.execute("SELECT * FROM genres")
     # その他を削除する
